7fd8c749821ad88d44abadd60892c4b6923e7e19
Mingi Hong <mingi.hong@tu-berlin.de>
2025-03-30 19:56:45 +0900

    No Popular bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ function App() {
   return (
     <Router>
       <Header />
-      <PopularSidebar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<WelcomePage />} />
